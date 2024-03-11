b62dfdbf993c134e9b0e1acf8c0721102f4f7422
mohammadashrafulislam <ixdell41@gmail.com>
2024-03-11 19:51:34 +0600

    project list with client is working

--- src/AdminDashboard/ProjectSubmissionForm/ProjectSubmissionForm .jsx
+++ src/AdminDashboard/ProjectSubmissionForm/ProjectSubmissionForm .jsx
@@ -3,13 +3,14 @@ import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
 import { TagsInput } from 'react-tag-input-component';
 import Swal from 'sweetalert2';
+import { endPoint } from '../../forAll/forAll';
 
 const img_hosting_token = import.meta.env.VITE_img_upload_token;
 
 const ProjectSubmissionForm = () => {
   const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`;
-  const apiUrl = 'http://localhost:5000/projects'; // Replace with your actual API endpoint
-
+  const apiUrl = `${endPoint}/projects`; // Replace with your actual API endpoint
+  const clientApiUrl = `${endPoint}/clients`;
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [projectCategory, setProjectCategory] = useState('');
@@ -18,15 +19,32 @@ const ProjectSubmissionForm = () => {
   const [technologies, setTechnologies] = useState([]);
   const [duration, setDuration] = useState('');
   const [challenges, setChallenges] = useState('');
-  const [userName, setUserName] = useState('');
-  const [userEmail, setUserEmail] = useState('');
+  const [userName, setUserName] = useState('Md Ashraful Islam');
+  const [userEmail, setUserEmail] = useState('mohammadashrafulislam33@gmail.com');
   const [projectImage, setProjectImage] = useState(null); // State for project image
   const [galleryImages, setGalleryImages] = useState([]); // State for gallery images
+  
+  const [clientInfo, setClientInfo] = useState({
+    userName: '',
+    userEmail: '',
+    userSocialMedia: ''
+  });
+  const [isFeatured, setIsFeatured] = useState(false);
+  const [mobileImage, setMobileImage] = useState(null); 
+  const [tabletImage, setTabletImage] = useState(null);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     let projectImgURL = '';
+    let mobileImgURL = '';
+    let tabletImgURL = '';
     let galleryImgURLs = [];
+    const clientData = {
+      userName: clientInfo.userName,
+      userEmail: clientInfo.userEmail,
+      userSocialMedia: clientInfo.userSocialMedia
+    };
+  
 
     if (projectImage) {
       const formData = new FormData();
@@ -46,7 +64,7 @@ const ProjectSubmissionForm = () => {
         console.error('Error uploading project image:', error);
       }
     }
-
+  
     if (galleryImages.length > 0) {
       try {
         for (const imageFile of galleryImages) {
@@ -69,10 +87,51 @@ const ProjectSubmissionForm = () => {
         console.error('Error uploading gallery images:', error);
       }
     }
+    if (mobileImage) {
+      const formData = new FormData();
+      formData.append('image', mobileImage);
+    
+      try {
+        const response = await fetch(img_hosting_url, {
+          method: 'POST',
+          body: formData,
+        });
+        const imgResponse = await response.json();
+        console.log('Mobile Image Upload Response:', imgResponse);
+        if (imgResponse.success) {
+          mobileImgURL = imgResponse.data.display_url;
+        }
+      } catch (error) {
+        console.error('Error uploading mobile image:', error);
+      }
+    }
+    
+    if (tabletImage) {
+      const formData = new FormData();
+      formData.append('image', tabletImage);
+    
+      try {
+        const response = await fetch(img_hosting_url, {
+          method: 'POST',
+          body: formData,
+        });
+        const imgResponse = await response.json();
+        console.log('Tablet Image Upload Response:', imgResponse);
+        if (imgResponse.success) {
+          tabletImgURL = imgResponse.data.display_url;
+        }
+      } catch (error) {
+        console.error('Error uploading tablet image:', error);
+      }
+    }
+    
+    
 
     const data = {
       title,
       description,
+      mobileImage: mobileImgURL,
+      tabletImage: tabletImgURL,
       projectCategory,
       projectUrl,
       githubUrl,
@@ -83,9 +142,22 @@ const ProjectSubmissionForm = () => {
       userEmail,
       projectImage: projectImgURL,
       galleryImages: galleryImgURLs,
+      clientInfo,
+      isFeatured
     };
+    
 
     try {
+      const clientResponse = await fetch(clientApiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(clientData),
+    });
+
+    const clientResult = await clientResponse.json();
+    console.log('Client API response:', clientResult);
       const response = await fetch(apiUrl, {
         method: 'POST',
         headers: {
@@ -130,12 +202,23 @@ const ProjectSubmissionForm = () => {
         setUserEmail('');
         setProjectImage(null);
         setGalleryImages([]);
+        setMobileImage(null);
+        setTabletImage(null);
+        
       }
     } catch (error) {
       console.error('Error submitting project:', error);
     }
   };
+  const handleMobileImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setMobileImage(selectedImage);
+  };
 
+  const handleTabletImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setTabletImage(selectedImage);
+  };
   const handleProjectImageChange = (e) => {
     const selectedImage = e.target.files[0];
     setProjectImage(selectedImage);
@@ -145,11 +228,29 @@ const ProjectSubmissionForm = () => {
     const selectedImages = e.target.files;
     setGalleryImages(Array.from(selectedImages));
   };
+  
 
   return (
     <div className='w-full md:w-3/4 mx-auto p-10'>
       <h2 className="md:text-3xl font-bold my-6 text-center text-white text-2xl">Project Submission Form</h2>
       <form onSubmit={handleFormSubmit}>
+      <hr />
+        <div className="my-4">
+          <label htmlFor="isFeatured" className="block text-gray-200 text-sm font-bold mb-2">
+            Featured Project:
+          </label>
+          <div className='flex'>
+          <input
+            type="checkbox"
+            id="isFeatured"
+            checked={isFeatured}
+            onChange={() => setIsFeatured(!isFeatured)}
+            className="mr-2 toggle"
+          />
+          <span className="text-gray-200">Is this project featured?</span>
+          </div>
+        </div>
+        <hr />
         <div className="my-4">
           <label htmlFor="title" className="block text-gray-200 text-sm font-bold mb-2">
             Project Title:
@@ -190,6 +291,31 @@ const ProjectSubmissionForm = () => {
           />
         </div>
         <hr />
+        <div className="my-4">
+        <label htmlFor="mobileImage" className="block text-gray-200 text-sm font-bold mb-2">
+          Mobile Image:
+        </label>
+        <input
+          type="file"
+          id="mobileImage"
+          accept="image/*"
+          onChange={handleMobileImageChange}
+          className="bg-white border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <div className="my-4">
+        <label htmlFor="tabletImage" className="block text-gray-200 text-sm font-bold mb-2">
+          Tablet Image:
+        </label>
+        <input
+          type="file"
+          id="tabletImage"
+          accept="image/*"
+          onChange={handleTabletImageChange}
+          className="bg-white border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <hr />
         <div className='my-4'>
           <label htmlFor='projectCategory' className='block text-gray-200 text-sm font-bold mb-2'>
             Project Category:
@@ -308,7 +434,6 @@ const ProjectSubmissionForm = () => {
           <input
             type="text"
             id="userName"
-            defaultValue={"Md Ashraful Islam"}
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
             required
@@ -322,13 +447,53 @@ const ProjectSubmissionForm = () => {
           <input
             type="email"
             id="userEmail"
-            defaultValue={"mohammadashrafulislam33@gmail.com"}
             value={userEmail}
             onChange={(e) => setUserEmail(e.target.value)}
             required
             className="w-full bg-white border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
           />
         </div>
+        <hr />
+        <h1 className='text-white text-center font-bold mt-6 text-3xl'>CLIENT INFO</h1>
+        <div className="my-4">
+        <label htmlFor="userName" className="block text-gray-200 text-sm font-bold mb-2">
+          Client Name:
+        </label>
+        <input
+          type="text"
+          id="userName"
+          value={clientInfo.userName}
+          onChange={(e) => setClientInfo({ ...clientInfo, userName: e.target.value })}
+          required
+          className="w-full bg-white border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <div className="my-4">
+        <label htmlFor="userEmail" className="block text-gray-200 text-sm font-bold mb-2">
+          Client Email:
+        </label>
+        <input
+          type="email"
+          id="userEmail"
+          value={clientInfo.userEmail}
+          onChange={(e) => setClientInfo({ ...clientInfo, userEmail: e.target.value })}
+          required
+          className="w-full bg-white border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <div className="my-4">
+        <label htmlFor="userSocialMedia" className="block text-gray-200 text-sm font-bold mb-2">
+          Client Social:
+        </label>
+        <input
+          type="social"
+          id="userSocialMedia"
+          value={clientInfo.userSocialMedia}
+          onChange={(e) => setClientInfo({ ...clientInfo, userSocialMedia: e.target.value })}
+          required
+          className="w-full bg-white border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+        />
+      </div>
         <button type="submit" className="number-slide1 text-white py-3 px-4 hover:bg-blue-600 w-full my-4">
           Submit
         </button>
--- src/AdminDashboard/ProjectsList/ProjectsList.jsx
+++ src/AdminDashboard/ProjectsList/ProjectsList.jsx
@@ -1,10 +1,83 @@
+import React, { useState, useEffect } from 'react';
 
 const ProjectsList = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [projects, setProjects] = useState([]);
+  const [clientData, setClientData] = useState([]);
+  const clientId = projects?.map(project => project.clientInfo);
+      const fetchClientInfo = async (clientId) => {
+      try {
+        const response = await fetch(`http://localhost:5000/clients/${clientId}`);
+        const data = await response.json();
+        setClientData(data);
+      } catch (error) {
+        console.error('Error fetching client data:', error);
+        return null;
+      }
+    };
+    fetchClientInfo(clientId)
+  useEffect(() => {
+    // Fetch data from the API when the component mounts
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/projects');
+        const data = await response.json();
+        setProjects(data); // Assuming the API returns an array of projects
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+
+  return (
+    <div className="overflow-x-auto">
+    {projects?.map((project, index) =>(<table className="table text-white" key={index}>
+      {/* head */}
+      <thead className='text-white'>
+        <tr>
+          <th className='pr-0 w-[2%] text-center'>No</th>
+          <th className='w-[65%] text-center'>Projects</th>
+          <th className='w-[15%] text-center '>Clients</th>
+          <th className='w-[18%] text-center'>Controller</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* row 1 */}
+        <tr className='text-white'>
+          <td className='pr-0 w-[2%] font-bold text-center'>{index+1}</td>
+          <td className='w-[65%]'>
+            <div className="flex items-center gap-3">
+              <div className="avatar mx-0">
+                <div className="w-20 h-12 md:w-32 md:h-20">
+                  <img src={project.projectImage} alt="Md Ashraful Islam Portfolio Showcase" className="w-full rounded" />
+                </div>
+              </div>
+              <div>
+                <div className="font-bold">{project.title}</div>
+            {project.technologies?.map((technology, index) =>(
+                <span className="badge badge-ghost badge-sm mx-1" key={index}>{technology}</span>
+            ))}               
+              </div>
+            </div>
+          </td>
+          <td className='w-[15%] text-center'>
+           {clientData?.userName}
+            <br/>
+            <div className="text-sm opacity-50 mb-1">{clientData?.userEmail}</div>
+            <a href={`${clientData?.userSocialMedia}`}><button className='btn-primary btn btn-xs'>Social</button></a>
+          </td>
+          <td className='w-[18%]'>
+            <button className="btn btn-success text-white btn-xs md:mr-1 mb-2">Update</button>
+            <button className="btn btn-error text-white btn-xs">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>))}
+  </div>
+  );
 };
 
 export default ProjectsList;
