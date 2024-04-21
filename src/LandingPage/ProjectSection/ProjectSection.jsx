0891eb27e6d8a3505c4cac8fde17f68e8915846b
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-21 10:31:11 +0600

    getting portfolio data from home page

--- src/LandingPage/ProjectSection/ProjectSection.jsx
+++ src/LandingPage/ProjectSection/ProjectSection.jsx
@@ -1,9 +1,26 @@
 
+import { useState, useEffect } from "react";
+import { endPoint } from "../../forAll/forAll";
+
 const ProjectSection = () => {
+  const [projects, setProjects] = useState(null)
+  useEffect(()=>{
+    const fetchData = async()=>{
+     const projectsResponse = await fetch(`${endPoint}/projects`);
+     const projectsData = await projectsResponse.json();
+     setProjects(projectsData)
+    }
+    fetchData()
+  },[])
     return (
         <div className="my-16">
       <h6 className="text-white text-2xl mb-5">Projects --</h6>
+  {
+    projects?.map((project, index) => <div className="card w-96 glass" key={index}>
+  <figure><img className="" src={project.projectImage} alt="car!"/></figure>
 
+</div>)
+  }
             
         </div>
     );
