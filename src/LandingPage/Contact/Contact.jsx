3aaa8840f14da4f9e11ace32ea095a0d3311641c
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-24 00:16:57 +0600

    contact info

--- src/LandingPage/Contact/Contact.jsx
+++ src/LandingPage/Contact/Contact.jsx
@@ -4,15 +4,15 @@ import { motion } from 'framer-motion';
 const Contact = () => {
   return (
     <div className="my-16">
-    <h3 className="text-2xl mb-4 text-black">Contact Info</h3>
-    <div className="text-black md:flex gap-5 mb-10">
+    <h3 className="text-2xl mb-4 text-white">Contact Info</h3>
+    <div className="text-white md:flex justify-center gap-5 mb-10">
     <motion.div
       initial={{ opacity: 0, x: -20 }}
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.5, delay: 0.2 }}
       className="flex items-center mb-4 contact-info-area"
     >
-      <FaMapMarkedAlt className="text-black mr-2" />
+      <FaMapMarkedAlt className="text-white mr-2" />
       <p>Sylhet, Bangladesh</p>
     </motion.div>
     <motion.div
@@ -21,7 +21,7 @@ const Contact = () => {
       transition={{ duration: 0.5, delay: 0.4 }}
       className="flex items-center mb-4 contact-info-area"
     >
-      <FaEnvelope className="text-black mr-2" />
+      <FaEnvelope className="text-white mr-2" />
       <p>mohammadashrafulislam33@gmail.com</p>
     </motion.div>
     <motion.div
@@ -30,23 +30,23 @@ const Contact = () => {
       transition={{ duration: 0.5, delay: 0.6 }}
       className="flex items-center contact-info-area"
     >
-      <FaPhone className="text-black mr-2" />
+      <FaPhone className="text-white mr-2" />
       <p>+8801747274852</p>
     </motion.div>
   </div>
-      <h6 className="text-black text-2xl mb-5">Contact --</h6>
+      <h6 className="text-white text-2xl mb-5">Contact --</h6>
       <div className="md:flex justify-center">
-        <div className="w-[50%] contact-area rounded-3xl p-8">
+        <div className="w-[50%] bg-gradient-to-r from-[#2121214b] to-[#111111] rounded-3xl p-8">
           <form action="https://formspree.io/f/mgejeqoy" method="POST">
             <div className="mb-4">
-              <label className="block text-black text-sm font-medium mb-2" htmlFor="name">
+              <label className="block text-white text-sm font-medium mb-2" htmlFor="name">
                 Name
               </label>
               <motion.input
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.2 }}
-                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-black py-2 px-4"
+                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-white py-2 px-4"
                 type="text"
                 id="name"
                 placeholder="Type your name"
@@ -55,14 +55,14 @@ const Contact = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-black text-sm font-medium mb-2" htmlFor="email">
+              <label className="block text-white text-sm font-medium mb-2" htmlFor="email">
                 Email
               </label>
               <motion.input
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.4 }}
-                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-black py-2 px-4"
+                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-white py-2 px-4"
                 type="email"
                 placeholder="Your Email"
                 name="email"
@@ -70,11 +70,11 @@ const Contact = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-black text-sm font-medium mb-2" htmlFor="service">
+              <label className="block text-white text-sm font-medium mb-2" htmlFor="service">
                 Service Needed
               </label>
               <select
-                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-black py-2 px-4"
+                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-white py-2 px-4"
                 id="service"
                 name="service"
               >
@@ -86,11 +86,11 @@ const Contact = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-black text-sm font-medium mb-2" htmlFor="website">
+              <label className="block text-white text-sm font-medium mb-2" htmlFor="website">
                 Website
               </label>
               <input
-                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-black py-2 px-4"
+                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-white py-2 px-4"
                 type="text"
                 id="website"
                 placeholder="Your Website"
@@ -99,11 +99,11 @@ const Contact = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-black text-sm font-medium mb-2" htmlFor="best-time">
+              <label className="block text-white text-sm font-medium mb-2" htmlFor="best-time">
                 Best Time & Date to Contact
               </label>
               <input
-                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-black py-2 px-4"
+                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-white py-2 px-4"
                 type="datetime-local"
                 id="best-time"
                 name="best-time"
@@ -111,11 +111,11 @@ const Contact = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-black text-sm font-medium mb-2" htmlFor="phone">
+              <label className="block text-white text-sm font-medium mb-2" htmlFor="phone">
                 Phone Number
               </label>
               <input
-                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-black py-2 px-4"
+                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-white py-2 px-4"
                 type="tel"
                 id="phone"
                 placeholder="Your Phone Number"
@@ -124,11 +124,11 @@ const Contact = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-black text-sm font-medium mb-2" htmlFor="contact-method">
+              <label className="block text-white text-sm font-medium mb-2" htmlFor="contact-method">
                 Preferred Contact Method
               </label>
               <select
-                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-black py-2 px-4"
+                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-white py-2 px-4"
                 id="contact-method"
                 name="contact-method"
               >
@@ -139,11 +139,11 @@ const Contact = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-black text-sm font-medium mb-2" htmlFor="subject">
+              <label className="block text-white text-sm font-medium mb-2" htmlFor="subject">
                 Subject
               </label>
               <input
-                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-black py-2 px-4"
+                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-white py-2 px-4"
                 type="text"
                 id="subject"
                 placeholder="Subject"
@@ -152,14 +152,14 @@ const Contact = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-black text-sm font-medium mb-2" htmlFor="message">
+              <label className="block text-white text-sm font-medium mb-2" htmlFor="message">
                 Message
               </label>
               <motion.textarea
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.6 }}
-                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-black py-2 px-4"
+                className="w-full bg-transparent rounded-md border border-white focus:outline-none focus:ring-2 focus:ring-white text-white py-2 px-4"
                 id="message"
                 rows="4"
                 placeholder="What's on your mind?"
