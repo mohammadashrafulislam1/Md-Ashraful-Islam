811a74b71a97f3645eaa685a006df5e11b695271
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-24 19:44:13 +0600

    Navigation is responsive for mobile

--- src/Shared/Navigation/Navigation.jsx
+++ src/Shared/Navigation/Navigation.jsx
@@ -12,10 +12,10 @@ const Navigation = () => {
         <div className="navbar">
   <div className="navbar-start">
     <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+      <label tabIndex={0} className="text-white lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="dropdown-content mt-3 z-[1] p-2 shadow bg-[#0f0f0f00] rounded-box w-52 text-[#494949] border border-black">
+      <ul tabIndex={0} className="dropdown-content mt-3 z-[1] p-2 shadow bg-[#0f0f0f00] rounded-box w-52 text-[#ffffff] border border-black bg-black">
         {navbar}
       </ul>
     </div>
