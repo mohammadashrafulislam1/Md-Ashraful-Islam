6c30e843de12ff1700b61bb0c9ff77518f1110ef
mohammadashrafulislam <ixdell41@gmail.com>
2024-06-18 06:28:13 +0600

    Project Section CSS

--- src/LandingPage/ProjectSection/ProjectSection.jsx
+++ src/LandingPage/ProjectSection/ProjectSection.jsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { endPoint } from "../../forAll/forAll";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { Container, Grid, Box, Typography, Button } from '@mui/material';
+import {FaArrowRight} from 'react-icons/fa';
 
 const truncateDescription = (description, limit) => {
   const words = description.split(' ');
@@ -14,6 +16,7 @@ const truncateDescription = (description, limit) => {
 const ProjectSection = () => {
   const [projects, setProjects] = useState(null);
   const [filteredProjects, setFilteredProjects] = useState(null);
+  console.log(projects)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,71 +36,52 @@ const ProjectSection = () => {
     if (category === "All") {
       setFilteredProjects(projects);
     } else {
-      const filterProject = projects.filter(project => project.projectCategory === category);
+      const filterProject = projects.filter(project => project.category === category);
       setFilteredProjects(filterProject);
     }
   };
 
   return (
-    <div className="mt-16 project-section text-white overflow-hidden">
-      <h6 className="text-white text-2xl mb-5 md:mb-12 pt-10 md:pt-0">Projects --</h6>
-      <div className="categories-container">
-        <div className="md:flex flex gap-2 justify-center mb-10">
-          <p className="tag-area" onClick={() => handleCategory("All")}>All</p>
-          <p className="tag-area" onClick={() => handleCategory("Full Stack Web Development")}>Full Stack Web Development</p>
-          <p className="tag-area" onClick={() => handleCategory("CMS")}>CMS</p>
-          <p className="tag-area" onClick={() => handleCategory("Search Engine Optimization")}>SEO</p>
-          <p className="tag-area" onClick={() => handleCategory("Web Design")}>Web Design</p>
-          <p className="tag-area" onClick={() => handleCategory("Video Editing")}>Video Editing</p>
-        </div>
-      </div>
-      <div className="md:pb-10 pb-20">
-        {filteredProjects?.map((project, index) => (
-          <div key={index} className="project-container mb-8 p-4 rounded-lg shadow-lg bg-gray-800 flex justify-center">
-            <div className="">
-                <img className="rounded-md w-[500px] h-[500px]" src={project.projectImage} alt={`Project Image ${project.title}`} />
-            </div>
-            {/* body */}
-            <div>
-            <div className="body-area flex flex-col justify-between">
-              <div>
-                <h1 className="text-2xl uppercase font-medium">{project.title}</h1>
-                <div className="font-[300]" dangerouslySetInnerHTML={{ __html: truncateDescription(project.description, 30) }} />
-                <p className="font-[600] text-blue-400">Project Category: <span className="font-[300]">{project.projectCategory}</span></p>
-                <div className="flex gap-4 mt-2">
-                  <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
-                    <p className="tag-area md:text-[14px] text-[12px]">GitHub</p>
-                  </a>
-                  <a href={project.projectUrl} target="_blank" rel="noopener noreferrer">
-                    <p className="tag-area md:text-[14px] text-[12px]">Live Website</p>
-                  </a>
-                </div>
-                <div>
-                  <p className="mt-2">Project Duration: {project.duration} days</p>
-                </div>
-              </div>
-              <div className="flex md:justify-end justify-center lg:mt-2 mb-[-6px]">
-                <Link to={`/projects/${project.id}`}>
-                  <button className="button-rounded mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    <p>View</p>
-                  </button>
-                </Link>
-              </div>
-            </div>
-            <div className="categories-container">
-              <div className="md:flex flex flex-nowrap gap-1 md:gap-2 lg:gap-5 lg:bottom-12 lg:right-28 font-[100] text-[12px] md:text-[14px] lg:text-[16px] bottom-[1] ml-4 md:right-0 md:bottom-8">
-                {project.technologies && JSON.parse(project.technologies).slice(0, 5).map((technology, index) => (
-                  <div key={index} className="tag-area">
-                    <p className="font-light">{technology}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Box id="projects" sx={{ py: 8 }}>
+      <Container maxWidth="lg">
+        <Typography variant="h1" sx={{ mb: 4 }}>
+          projects that i built_
+        </Typography>
+        <Grid container spacing={4}>
+          {filteredProjects && filteredProjects.map((project, index) => (
+            <Grid item xs={12} sm={12} md={6} key={index}>
+              <Box sx={{ boxShadow: 3, borderRadius: 2, p: 2 }}>
+                <Typography variant="h2" className="gradient-text" sx={{ mb: 2 }}>
+                  {project.title.split('<br>').map((line, index) => (
+                    <React.Fragment key={index}>{line}<br /></React.Fragment>
+                  ))}
+                </Typography>
+                <Box className="flip-effect">
+                  <img
+                    alt={project.title}
+                    src={project.projectImage
+                    }
+                    width="238"
+                    height="139"
+                    style={{ color: 'transparent', objectFit: 'cover', height: '140px' }}
+                  />
+                </Box>
+                <Box>
+                  <FaArrowRight />
+                </Box>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+        <Box sx={{ mt: 4 }}>
+          <Link to="/home/project">
+            <Button variant="contained" color="primary">
+              All projects
+            </Button>
+          </Link>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
