6fe1d8d47481d5c9ed4574795e59f2260d341272
Md Ashraful Islam <90489175+mohammadashrafulislam1@users.noreply.github.com>
2024-10-14 17:40:38 +0600

    Update Footer.jsx

--- src/LandingPage/Footer/Footer.jsx
+++ src/LandingPage/Footer/Footer.jsx
@@ -16,9 +16,9 @@ const Footer = () => {
         <a className="normal-case text-xl text-white text-center flex justify-center mb-4 flex justify-center items-center gap-1 font-semibold"><span className="bg-[#0051ff] p-1">Md</span> Ashraf</a>
         <div className="text-white">{navbar}</div>
         <div className="flex justify-center gap-6 mt-3">
-            <a href="#" className="text-[#414141] text-xl"><FaFacebookF/></a>
-            <a href="#" className="text-[#414141] text-xl"><FaLinkedinIn/></a>
-            <a href="#" className="text-[#414141] text-xl"><FaInstagram/></a>
+            <a href="https://www.facebook.com/profile.php?id=100013514229148" className="text-[#414141] text-xl"><FaFacebookF/></a>
+            <a href="https://www.linkedin.com/in/mdashrafulislam12" className="text-[#414141] text-xl"><FaLinkedinIn/></a>
+            <a href="https://www.instagram.com/the_ash_raf_" className="text-[#414141] text-xl"><FaInstagram/></a>
         </div>
     </div>
     );
